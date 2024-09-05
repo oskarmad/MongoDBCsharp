74054ec7e1a5f90e9fb08ab04baf01f14829e41a
Oscar Mauricio Diaz <oskarmad@gmail.com>
2024-09-05 12:08:12 -0500

    Initial commit

---
+++ MongoDBCsharp/Models/People.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBCsharp.Models
+{
+    public class People
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+
+        [BsonElement("edad")]
+        public int Edad { get; set; }
+
+        [BsonElement("country")]
+        public string Country { get; set; }
+    }
+}
---
+++ MongoDBCsharp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Driver;
+using MongoDBCsharp.Models;
+
+Console.WriteLine("Hello, World!");
+
+MongoClient client = new MongoClient("mongodb://localhost:27017");
+//mongodb+srv://oskarmax:password@omdcluster.u6zyq.mongodb.net/
+IMongoDatabase database = client.GetDatabase("school");
+
+IMongoCollection<People> peopleDB = database.GetCollection<People>("people");
+
+//Crear
+People People = new People() { Nombre = "Oscar", Edad = 35, Country = "Colombia" };
+//People People = new People() { Nombre = "Emmma", Edad = 5 };
+//People People = new People() { Nombre = "Annie", Edad = 9 };
+peopleDB.InsertOne(People);
+
+//Consultar todo
+List<People> list = peopleDB.Find(d => true).ToList();
+
+list.First().Nombre = "Emmma Díaz";
+
+//Edito el primero por el id
+peopleDB.ReplaceOne(d => d.Id == list.First().Id, list.First());
+
+//eliminar
+//peopleDB.DeleteOne(d => d.Id == list.First().Id);
+
+
+Console.WriteLine("Fin!");
